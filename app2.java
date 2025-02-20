bb6d2c1b1ccd78491ad7c47676f21640bf142bd1
essounni aya <essounniaya@gmail.com>
2025-02-20 17:08:18 +0100

    ex1 terminé

---
+++ app2.java
@@ -0,0 +1,24 @@
+
+ import java.util.Scanner;
+ public class app2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int nl,nc,i,j,s;
+        int[][] M=new int[30][30];
+        System.out.println("donner le nombre de ligne");
+        nl=sc.nextInt();
+        while (nl>30){
+            System.out.println("donner le nombre de ligne");
+            nl=sc.nextInt();
+            }
+            System.out.println("donner le nombre de colonne");
+            nc=sc.nextInt();
+            while (nc>30){
+                System.out.println("donner le nombre de colonne");
+                nc=sc.nextInt();
+            }
+            System.out.println("remplir la matrice");
+            for(i=0;i<nl)
+        }
+    
+}
